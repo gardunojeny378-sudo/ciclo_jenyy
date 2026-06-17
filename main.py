2374323f6f4833ea3eaef9147d9c4c3212f6838c
gardunojeny378-sudo <gardunojeny378@gmail.com>
2026-06-16 20:05:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ app = Flask(__name__, template_folder='.', static_folder='static')
 
 @app.route('/')
 def home():
-    return render_template('index.html.txt')
+    return render_template('index.html')
 
 @app.route('/api/alerta', methods=['POST'])
 def alerta():
